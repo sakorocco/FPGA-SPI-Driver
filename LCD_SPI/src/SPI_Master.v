8e72e31b4a71c8d997e71a68c3832b3c351aa266
Sarkis Chekerjian <sako.chekerjian.636@my.csun.edu>
2023-12-01 12:17:53 -0800

    Made code readable, creating a testbench

--- LCD_SPI/src/SPI_Master.v
+++ LCD_SPI/src/SPI_Master.v
@@ -44,57 +44,104 @@ module SPI_Master(SCLK, CS, CLK, rst, btn1, MOSI, DC, RES, VCC_EN, PMOD_EN, led)
                         .i_signal (btn_deb[0]),
                         .o_signal (btn_pulse[0]) 
     );
+    
 always @(posedge CLK or posedge btn_pulse[0]) begin
     if (btn_pulse[0]) begin
         curr_state = IDLE;
-        led = curr_state;
-        temp = 4'b0000;
-        CS = 1'b1;
-        SCLK = 1'b0;
-        MOSI = 1'b0;
-        transmit_data = 1'b0;
-        shift_register = 8'b0;
-    end else begin
-        if(curr_state == IDLE) begin
-            CS = 1'b1;
-            SCLK = 1'b0;
-            led = curr_state;
-            DC = 1'b0;              //Set DC logic low
-            if(btn_pulse[1]) begin
-                //Assert CS and begin transmission of data
-                CS = 1'b0;
-                shift_register = 16'hAA; //shift register takes data
-                curr_state = SHIFT;
-                led = curr_state;
-                transmit_data = 1'b1;
-                
-             end
-          
-    end else begin
-        if(curr_state == SHIFT) begin
-        SCLK = ~SCLK;
-        led = curr_state;
-        
-            if (SCLK == 1'b1) begin
-            //Shift out data on rising edge of SCLK
-            MOSI = shift_register[7]; //MSB first
-            //Shift bits to left and replace LSB with 0
-            shift_register = {shift_register[6:0], 1'b0};
-            end
-            
-            if(transmit_data) begin
-                //If all data sent, all 0s
-                if(shift_register == 8'b0) begin
-                    //transmission complete
-                    curr_state = IDLE;
-                    transmit_data = 1'b0; 
-                end                    
-            end
-            
-        end  
-        end   
     end
+    if(btn_pulse[1]) begin
+        curr_state = SHIFT;
+    end    
+end
 
-end 
+always @(posedge CLK) begin
+    case(curr_state )
+        IDLE: begin
+                led = curr_state; 
+                CS = 1'b1;
+                SCLK = 1'b0;  
+                MOSI = 1'b0;
+                transmit_data = 1'b0;
+                shift_register = 8'b0;        
+              end
+
+        SHIFT: begin
+                led = curr_state;
+                CS = 1'b0;
+                SCLK = ~SCLK;
+                shift_register = 16'hAA; //shift register takes data
+                transmit_data = 1'b1;    
+               end
+    endcase
+end
+
+always @(posedge SCLK) begin
+    //Shift out data on rising edge of SCLK
+    MOSI = shift_register[15]; //MSB first
+    //Shift bits to left and replace LSB with 0
+    shift_register = {shift_register[14:0], 1'b0};
+end
+
+always @(transmit_data) begin
+    //If all data sent, all 0s
+    if(shift_register == 8'b0) begin
+        //transmission complete
+        curr_state = IDLE;
+        transmit_data = 1'b0; 
+    end                    
+end
+
+//always @(posedge CLK or posedge btn_pulse[0]) begin
+//    if (btn_pulse[0]) begin
+//        curr_state = IDLE;
+//        led = curr_state;
+//        temp = 4'b0000;
+//        CS = 1'b1;
+//        SCLK = 1'b0;
+//        MOSI = 1'b0;
+//        transmit_data = 1'b0;
+//        shift_register = 8'b0;
+//    end else begin
+//        if(curr_state == IDLE) begin
+//            CS = 1'b1;
+//            SCLK = 1'b0;
+//            led = curr_state;
+//            DC = 1'b0;              //Set DC logic low
+//            if(btn_pulse[1]) begin
+//                //Assert CS and begin transmission of data
+//                CS = 1'b0;
+//                shift_register = 16'hAA; //shift register takes data
+//                curr_state = SHIFT;
+//                led = curr_state;
+//                transmit_data = 1'b1;
+                
+//             end
+          
+//    end else begin
+//        if(curr_state == SHIFT) begin
+//        SCLK = ~SCLK;
+//        led = curr_state;
+        
+//            if (SCLK == 1'b1) begin
+//            //Shift out data on rising edge of SCLK
+//            MOSI = shift_register[7]; //MSB first
+//            //Shift bits to left and replace LSB with 0
+//            shift_register = {shift_register[6:0], 1'b0};
+//            end
+            
+//            if(transmit_data) begin
+//                //If all data sent, all 0s
+//                if(shift_register == 8'b0) begin
+//                    //transmission complete
+//                    curr_state = IDLE;
+//                    transmit_data = 1'b0; 
+//                end                    
+//            end
+            
+//        end  
+//        end   
+//    end
+
+//end 
 
 endmodule
